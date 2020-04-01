01a83bae53422f5f1bb476c838ebb460e1400a82
DecoyTNT <42523466+DecoyTNT@users.noreply.github.com>
2020-04-01 15:32:49 -0600

    Creando el formulario

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import Formulario from "./components/Formulario";
 
 function App() {
-    return <h1>Administrador de pacientes</h1>;
+  return (
+    <Fragment>
+      <h1>Administrador de pacientes</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario />
+          </div>
+          <div className="one-half column">2</div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/Formulario.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react';
+
+const Formulario = () => {
+    return (
+        <Fragment>
+            <h2>Crear Cita</h2>
+
+            <form>
+                <label>Nombre Mascota</label>
+                <input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" />
+                <label>Nombre Dueño</label>
+                <input type="text" name="propietario" className="u-full-width" placeholder="Nombre Dueño de la mascota" />
+                <label>Fecha</label>
+                <input type="date" name="fecha" className="u-full-width" />
+                <label>Hora</label>
+                <input type="time" name="hora" className="u-full-width" />
+                <label>Síntomas</label>
+                <textarea className="u-full-width" name="sintomas"></textarea>
+
+                <button type="submit" className="u-full-width button-primary">
+                    Agregar Cita
+                </button>
+            </form>
+        </Fragment>
+    );
+};
+
+export default Formulario;
