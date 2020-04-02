dd0005548cd4ca37826de4aa28a1995d9f383487
DecoyTNT <42523466+DecoyTNT@users.noreply.github.com>
2020-04-01 18:04:29 -0600

    Validando el formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,21 +1,60 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Formulario = () => {
+    // Crear State de citas
+    const [cita, actualizarCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+    });
+
+    const [error, actualizarError] = useState(false);
+
+    // Función que se ejecuta cada que el usuario escribe en un input
+    const actualizarState = e => {
+        actualizarCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    // Extraer los valores
+    const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+    // Cuando se presiona agregar cita
+    const submitCita = e => {
+        e.preventDefault();
+
+        // Validar
+        if (mascota.trim() === '' || propietario.trim() === '' || fecha.trim() === '' || hora.trim() === '' || sintomas.trim() === '') {
+            return actualizarError(true);
+        }
+        actualizarError(false);
+
+        // Asignar un ID
+
+        console.log('Enviando');
+    };
+
     return (
         <Fragment>
             <h2>Crear Cita</h2>
 
-            <form>
+            {error ? <p className="alerta-error">Todos los campos son obligatorios</p> : null}
+
+            <form onSubmit={submitCita}>
                 <label>Nombre Mascota</label>
-                <input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" />
+                <input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" onChange={actualizarState} value={mascota} />
                 <label>Nombre Dueño</label>
-                <input type="text" name="propietario" className="u-full-width" placeholder="Nombre Dueño de la mascota" />
+                <input type="text" name="propietario" className="u-full-width" placeholder="Nombre Dueño de la mascota" onChange={actualizarState} value={propietario} />
                 <label>Fecha</label>
-                <input type="date" name="fecha" className="u-full-width" />
+                <input type="date" name="fecha" className="u-full-width" onChange={actualizarState} value={fecha} />
                 <label>Hora</label>
-                <input type="time" name="hora" className="u-full-width" />
+                <input type="time" name="hora" className="u-full-width" onChange={actualizarState} value={hora} />
                 <label>Síntomas</label>
-                <textarea className="u-full-width" name="sintomas"></textarea>
+                <textarea className="u-full-width" name="sintomas" onChange={actualizarState} value={sintomas}></textarea>
 
                 <button type="submit" className="u-full-width button-primary">
                     Agregar Cita
